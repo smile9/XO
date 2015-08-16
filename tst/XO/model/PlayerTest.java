e8f76107231ea12c6a8c20eacbb3c79a53c62ed5
Andrey <welikiy9@mail.ru>
2015-08-17 00:39:37 +0300

    Create simple test

---
+++ tst/XO/model/PlayerTest.java
@@ -0,0 +1,21 @@
+package XO.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Samsung on 17.08.2015.
+ */
+public class PlayerTest {
+
+    @Test
+    public void testGetName() throws Exception {
+
+    }
+
+    @Test
+    public void testGetFigure() throws Exception {
+
+    }
+}
